a32fad756aaa4b6f7308fcc8f71ff8da2c66fb32
Tri Phung Quang Minh <tri.phungquangminh@nashtechglobal.com>
2025-03-06 01:02:23 +0700

    Minor fixes

--- BlazorEcommerce/Data/Pagination/QueryableExtensions.cs
+++ BlazorEcommerce/Data/Pagination/QueryableExtensions.cs
@@ -5,6 +5,14 @@ namespace BlazorEcommerce.Data.Pagination
 {
     public static class QueryableExtensions
     {
+        /// <summary>
+        /// Simple pagination implementation.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="pageNumber"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
         public static async Task<PagedResultDTO<T>> ToPagedResultAsync<T>(
         this IQueryable<T> query, int pageNumber, int pageSize)
         {
@@ -12,6 +20,7 @@ namespace BlazorEcommerce.Data.Pagination
             if (pageSize < 1) pageSize = 10;
 
             var totalItems = await query.CountAsync();
+            // TODO Add Sorting support. Without sorting, the order of items is not guaranteed.
             var items = await query
                 .Skip((pageNumber - 1) * pageSize)
                 .Take(pageSize)
